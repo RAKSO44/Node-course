629d9218f778cb1e8518eace0e91de7a4a3d6321
RAKSO44 <oscarcruzaleguicastillo@gmail.com>
2025-11-06 18:00:50 -0500

    routing GET con node js

--- 5. routing/app.js
+++ 5. routing/app.js
@@ -17,15 +17,20 @@ function handleGETRequest(req, res) {
   const path = req.url;
 
   if (path === '/'){
-
     res.statusCode = 200;
     res.end(`I don't have a job yet, but this is another step to Australia`);
     
   } else if (path === '/cursos') {
-
     res.statusCode = 200;
     res.end(JSON.stringify(cursos.infoCursos));
+
+  } else if (path === '/cursos/programacion') {
+    res.statusCode = 200;
+    res.end(JSON.stringify(cursos.infoCursos.programacion));
   }
+
+  res.statusCode = 404;
+  res.end('El recurso solicitado no existe');
 }
 
 
