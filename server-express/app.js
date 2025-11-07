428802f248fa701de47dd36151ef527a9a3ab4b1
RAKSO44 <oscarcruzaleguicastillo@gmail.com>
2025-11-07 17:42:31 -0500

    routing GET with express

--- server-express/app.js
+++ server-express/app.js
@@ -3,11 +3,25 @@ const app = express();
 
 const { infoCursos } = require('./cursos');
 
-// Routing
-
+// Routing ----------------------------------------------
 app.get('/', (req, res) => {
   res.send(`I feel like I'm learning 💻`);
 });
+
+app.get('/api/cursos', (req, res) => {
+  res.send(JSON.stringify(infoCursos));
+});
+
+app.get('/api/cursos/programacion', (req, res) => {
+  res.send(JSON.stringify(infoCursos.programacion));
+});
+
+app.get('/api/cursos/matematicas', (req, res) => {
+  res.send(JSON.stringify(infoCursos.matematicas));
+});
+
+
+
 
 const PORT = process.env.PORT || 3000;
 
